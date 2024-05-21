5081a18a169b5cdedff127addf4a74ee9d37e134
jeonginh0 <stitch010913@gmail.com>
2024-05-21 18:15:48 +0900

    index.html (지도와 지도에 맛집검색 기능 구현) main.py (index.html)을 fastAPI 서버 웹사이트에서 렌더링

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect} from "react";
+
 
 function App() {
   return (
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI # FastAPI 애플리케이션을 생성하는데 사용
+from fastapi import FastAPI, Request, BackgroundTasks # FastAPI 애플리케이션을 생성하는데 사용
 from fastapi.templating import Jinja2Templates # FastAPI에서 Jinja2 템플릿 사용
-from fastapi import Request # 요청 객체 클래스
+from selenium_script import scrape_food_data
 import os # 운영체제와 상호작용
+
 
 app = FastAPI() # FastAPI()를 호출하여 FastAPI 객체를 생성
 
@@ -24,3 +25,5 @@ async def read_root(request: Request):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
