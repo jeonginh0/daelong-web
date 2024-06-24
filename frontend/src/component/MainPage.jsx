3f50663f9ef3b1153bb005f3a3102180bfe9ab17
jeonginh0 <stitch010913@gmail.com>
2024-06-25 00:04:52 +0900

    css 수정, mainpage 수정

--- frontend/src/component/MainPage.jsx
+++ frontend/src/component/MainPage.jsx
@@ -99,65 +99,69 @@ const MainPage = () => {
                 <div className="logo_wrapper">
                     <img src="/teamlogo.png" alt="로고" width="200px" height="90px"/>
                 </div>
-                <div className="myPageBtn" onClick={goMyPage}>마이페이지</div>
-                <div className="loginBtn" onClick={goLoginPage}>로그인</div>
-                <div className="signupBtn" onClick={goSignUpPage}>회원가입</div>
-            </div>
-            <div className="main_screen" style={{ overflow: 'auto' }}>
-                <img src="/map.png" alt="메인이미지" width="1920px" height="1900px"/>
-                <div className="text_screen">
-                    <div className="overlay">
-                        <h1 className="main_text">Meeting Service</h1>
-                        <p className="sub_text1">Team Do-Dam Web Application</p>
-                        <div className="expServiceBtn" onClick={() => setIsUsageModalOpen(true)}>
-                            사용방법
-                        </div>
-                        <div className="startServiceBtn" onClick={() => setIsStartModalOpen(true)}>
-                            사용하기
-                        </div>
+                <div className="signImg">
+                    <img src="/human.png" alt="sign Image" width="50px" height="50px"/>
+                    <div className="hoverBox">
+                        this is a box
                     </div>
                 </div>
-                {isUsageModalOpen && (
-                    <div className={`modal ${isUsageModalOpen ? 'show' : ''}`} ref={usageModalRef}>
-                        <div className="modal-background" onClick={() => setIsUsageModalOpen(false)}></div>
-                        <div className={`modal-content ${isUsageModalOpen ? 'show' : ''}`}>
-                            <span className="close" onClick={() => setIsUsageModalOpen(false)}>&times;</span>
-                            <p>여기에 사용 방법에 대한 내용을 작성합니다.</p>
+            </div>
+
+            <div className="main_screen" style={{overflow: 'auto'}}>
+                <img src="/mainpage_Img.png" alt="메인이미지" width="1920px" height="1080px"/>
+                <div className="text_screen">
+                    <div className="overlay">
+                            <h1 className="main_text">대롱대롱</h1>
+                            <p className="sub_text1">중간지점 찾기 및 일정관리 서비스</p>
+                            <div className="startServiceBtn" onClick={() => setIsStartModalOpen(true)}>
+                                사용하기
+                            </div>
                         </div>
                     </div>
-                )}
-                {isStartModalOpen && (
-                    <div className={`modal ${isStartModalOpen ? 'show' : ''}`} ref={startModalRef}>
-                        <div className="modal-background" onClick={() => setIsStartModalOpen(false)}></div>
-                        <div className={`modal-content ${isStartModalOpen ? 'show' : ''}`}>
-                            <span className="close" onClick={() => setIsStartModalOpen(false)}>&times;</span>
-                            {addresses.map((address, index) => (
-                                <div className="address-input-container" key={index}>
-                                    <input
-                                        type="text"
-                                        value={addresses[index]}
-                                        onChange={(e) => handleChange(index, e.target.value)}
-                                        placeholder="주소를 입력해주세요."
-                                        className="input-field" onClick={() => openAddressSearch(index)}
-                                    />
-                                    <button className="delete-button" onClick={() => handleRemoveAddress(index)}>-
-                                    </button>
-                                    <br/>
-                                </div>
-
-                            ))}
-                            <button className="add-button" onClick={handleAddAddress}>만날 사람 추가하기!</button>
-                            <button className="meet-button" onClick={handleSubmit}>여기서 만나자!</button>
+                    {isUsageModalOpen && (
+                        <div className={`modal ${isUsageModalOpen ? 'show' : ''}`} ref={usageModalRef}>
+                            <div className="modal-background" onClick={() => setIsUsageModalOpen(false)}></div>
+                            <div className={`modal-content ${isUsageModalOpen ? 'show' : ''}`}>
+                                <span className="close" onClick={() => setIsUsageModalOpen(false)}>&times;</span>
+                                <p>여기에 사용 방법에 대한 내용을 작성합니다.</p>
+                            </div>
                         </div>
+                    )}
+                    {isStartModalOpen && (
+                        <div className={`modal ${isStartModalOpen ? 'show' : ''}`} ref={startModalRef}>
+                            <div className="modal-background" onClick={() => setIsStartModalOpen(false)}></div>
+                            <div className={`modal-content ${isStartModalOpen ? 'show' : ''}`}>
+                                <span className="close" onClick={() => setIsStartModalOpen(false)}>&times;</span>
+                                {addresses.map((address, index) => (
+                                    <div className="address-input-container" key={index}>
+                                        <input
+                                            type="text"
+                                            value={addresses[index]}
+                                            onChange={(e) => handleChange(index, e.target.value)}
+                                            placeholder="주소를 입력해주세요."
+                                            className="input-field" onClick={() => openAddressSearch(index)}
+                                        />
+                                        <button className="delete-button" onClick={() => handleRemoveAddress(index)}>-
+                                        </button>
+                                        <br/>
+                                    </div>
 
-                    </div>
-                )}
-            </div>
-            <div>
-                <p className="sub_text2">daelongdaelong.official</p>
-            </div>
+                                ))}
+                                <button className="add-button" onClick={handleAddAddress}>만날 사람 추가하기!</button>
+                                <button className="meet-button" onClick={handleSubmit}>여기서 만나자!</button>
+                            </div>
+
+                        </div>
+                    )}
+                </div>
+                <div className="sub_screen" style={{overflow: 'auto'}}>
+                    <h1 style={{fontSize: '5em'}}>대롱대롱 사용방법</h1>
+                </div>
+                {/*<div>*/}
+                {/*    <p className="sub_text2">daelongdaelong.official</p>*/}
+                {/*</div>*/}
         </main>
-    );
+);
 };
 
 export default MainPage;
