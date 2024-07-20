b94b13872e32ed6cc367b11c32c58bbe20a11eb9
jeonginh0 <stitch010913@gmail.com>
2024-07-21 00:23:17 +0900

    07/21 프로젝트 마무리

---
+++ .venv/lib/python3.12/site-packages/legacy_test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import legacy
+
+class LegacyTest(unittest.TestCase):
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    def test_has_module(self):
+        self.assertEqual(legacy.has_module("sys"), True)
+        self.assertEqual(legacy.has_module("sys_error"), False)
+
+    def test_new_module(self):
+        import sys
+
+        value = legacy.new_module("sys_new")
+        self.assertEqual(type(value), type(sys))
+
+    def test_reload(self):
+        import sys
+
+        _sys = sys
+        sys = legacy.reload(sys)
+        self.assertEqual(sys, _sys)
+        self.assertEqual(id(sys), id(_sys))
