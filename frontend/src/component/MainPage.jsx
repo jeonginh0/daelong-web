246f85712cf79b9fcc03f8addbfb627ab47255cf
se1park <swon7150@gmail.com>
2024-07-20 22:35:30 +0900

    frontend-maybe-final

--- frontend/src/component/MainPage.jsx
+++ frontend/src/component/MainPage.jsx
@@ -57,6 +57,7 @@ const MainPage = () => {
             .then(coordsArray => {
                 const url = `/map?coords=${JSON.stringify(coordsArray)}`;
                 const token = localStorage.getItem('access_token'); // JWT 토큰 가져오기
+                navigate(url);
 
                 axios.post('http://localhost:8000/api/user/save-url/',
                     { page_url: url },
